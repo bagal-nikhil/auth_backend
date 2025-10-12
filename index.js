7f925d0f970e86ab4ac9f89c6e01efd7645b807a
Nikhil Bagal <nikhilbagalofficial@gmail.com>
2025-10-12 22:28:25 +0530

    Added morgan for logging and cors as well

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const cors = require("cors")
+const morgan = require("morgan")
 const app = express()
 const login = require("./routes/login/handler.js")
 const signup = require("./routes/signup/handler.js")
 const db = require("./database/index.js")
 const PORT = 3000;
 
+app.use(cors({
+    origin: "http://localhost:3001",
+    methods: ['GET', 'POST'],
+    credentials: true
+}))
 app.use(express.json());
+app.use(morgan("dev"))
 
 app.use("/auth/signup", signup);
 app.use("/auth/login", login);
