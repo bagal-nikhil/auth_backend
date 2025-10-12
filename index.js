714704125c342ae55d1be4a550a42b6d249a75e5
AmarPawar0405 <amarpawar0405@gmail.com>
2025-10-12 13:28:29 +0530

    Added /login route in index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const app =  express()
+const app = express()
+const login = require("./routes/login/handler")
 
 app.use(express.json());
+
+app.use("/login", login)
 
 app.get('/', (req, res) => {
     res.send("working the applicatio eninfnn")
