43c873639ccac377101744ad950929cda84f25d7
Nikhil Bagal <nikhilbagalofficial@gmail.com>
2025-10-12 13:02:37 +0530

    added handler.js files for files

---
+++ routes/login/handler.js
@@ -0,0 +1,10 @@
+const express =  require("express")
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    try {
+        console.log()
+    } catch (error) {
+        throw error;
+    }
+})
